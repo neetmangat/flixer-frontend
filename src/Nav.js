a86635755b128805cbeb32e5fb1f835ad4f2e4a0
neetmangat <neetmangat@gmail.com>
2021-02-02 00:11:11 -0600

    Added nav bar and transitions

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import logo from "./assets/images/flixer_logo.png";
 import "./Nav.css";
 
 function Nav() {
+  const [handleShow, setHandleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setHandleShow(true);
+      } else setHandleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="navbar">
-      <p>.</p>
+    <div className={`nav ${handleShow && "nav_black"}`}>
+      <img className="nav_logo" src={logo} alt="Flixer logo" />
+      <img
+        className="nav_avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Avatar"
+      />
     </div>
   );
 }
